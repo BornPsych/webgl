9421d2c01f9a8814c32e9103ac415d0be71ea8a4
Yogesh Shahi <65803503+BornPsych@users.noreply.github.com>
2024-04-11 03:33:27 +0530

    feat: added vercel page insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Webgl",
-  description: "will show webgl code here",
-};
+  title: 'Webgl',
+  description: 'will show webgl code here',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        {children} <SpeedInsights />
+      </body>
     </html>
-  );
+  )
 }
